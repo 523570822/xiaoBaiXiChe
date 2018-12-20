760cfa7057a46fe57f9044dddcb3e84b0e5bc0b3
wangjiaming <459681469@qq.com>
2018-12-20 13:07:56 +0800

    dwad

--- Application/Api/Controller/AgentController.class.php
+++ Application/Api/Controller/AgentController.class.php
@@ -32,7 +32,7 @@ class AgentController extends BaseController
      *Date:2018/12/18 16:22
      */
     public function login(){
-        $post = checkAppData('phone，password','手机号-密码');
+        $post = checkAppData('phone,password','账号-密码');
         /*$post['phone'] = 17622818248;
         $post['password'] = 123456;*/
         if (!isMobile($post['phone'])) {
