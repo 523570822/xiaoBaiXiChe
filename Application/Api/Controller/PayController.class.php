f1fda32ea35adcd94b3f9f7ae7d774aa1d386391
wangjiaming <459681469@qq.com>
2019-01-03 15:13:37 +0800

    提现类型2

--- Application/Api/Controller/PayController.class.php
+++ Application/Api/Controller/PayController.class.php
@@ -42,8 +42,19 @@ class PayController extends BaseController
         }
     }
 
+    /**
+     *提现类型
+     *user:jiaming.wang  459681469@qq.com
+     *Date:2019/01/03 14:26
+     */
     public function bankType(){
         $bank = M('BankType')->where(array('status'=>1))->field('bank_name,bank_pic')->select();
+        foreach ($bank as $k=>$v){
+            $pic['bank_pic'] = $v['bank_pic'];
+            $pic_path = getPicPath($pic['bank_pic']);
+            $bank[$k]['bank_pic_path'] = $pic_path;
+        }
+        var_dump($bank);exit;
         if(!empty($bank)){
             $this->apiResponse('1','成功',$bank);
         }else{
