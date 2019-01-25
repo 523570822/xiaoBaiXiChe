66847c90023141b0de58e41e5da684080e56fe59
谢吉发 <969318117@qq.com>
2019-01-25 09:31:03 +0800

    支付接口优化

--- Application/Api/Controller/IndexController.class.php
+++ Application/Api/Controller/IndexController.class.php
@@ -31,7 +31,7 @@ class IndexController extends BaseController
         $session_key = $openid->session_key;
         $openid = $openid->openid;
 
-        $this->apiResponse (1 , '成功' , $openid);
+        $this->apiResponse (1 , '成功' ,array ($openid,$session_key));
     }
 
     /**
@@ -114,56 +114,93 @@ class IndexController extends BaseController
         $this->apiResponse ('1' , '查询成功' , $list1);
     }
 
+
     /**
-     * 检验数据的真实性，并且获取解密后的明文.
-     * @param $encryptedData string 加密的用户数据
-     * @param $iv string 与用户数据一同返回的初始向量
-     * @param $data string 解密后的原文
-     * @return int 成功0，失败返回对应的错误码
+     * error code 说明.
+     * <ul>
+     *    <li>-41001: encodingAesKey 非法</li>
+     *    <li>-41003: aes 解密失败</li>
+     *    <li>-41004: 解密后得到的buffer非法</li>
+     *    <li>-41005: base64加密失败</li>
+     *    <li>-41016: base64解密失败</li>
+     * </ul>
      */
-    public function decryptData( $encryptedData, $iv, &$data,$sessionKey,$appid )
+    public static $OK = 0;
+    public static $IllegalAesKey = -41001;
+    public static $IllegalIv = -41002;
+    public static $IllegalBuffer = -41003;
+    public static $DecodeBase64Error = -41004;
+    // 小程序
+    public static $appid = 'wxf348bbbcc28d7e10';  //小程序appid
+    public static $secret = '2501eb21dd9346f91e9b612b0097b50f'; //小程序秘钥
+    public $sessionKey ='';
+    // 获取openId session-key 等
+    public function getopenIds($value='')
     {
-        if (strlen($sessionKey) != 24) {
-            return "1000";
+        $code = I('post.code');
+        $appid = self::$appid;
+        $secret = self::$secret;
+        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid='. $appid.'&secret='.$secret.'&js_code='.$code.'&grant_type=authorization_code';
+        $result = httpGet($url);
+        $res = json_decode($result);
+        session(['sessionKey'=>$res,'expire'=>7200]);
+        $this->ajaxReturn($res);
+    }
+
+    // 获取小程序手机号api 接口，对应下面小程序 js
+    public function getPhoneNumber1($value='')
+    {
+        $encryptedData = I('get.encryptedData');
+        $iv = I('get.iv');
+        $this->sessionKey=I('get.session_key');
+        $res = $this->decryptData($encryptedData, $iv);
+        // $res = json_decode($res);
+        if($res->phoneNumber){
+            // $res->phoneNumbe 就是手机号可以 写入数据库或者做其他操作
         }
-        $aesKey=base64_decode($sessionKey);
+        $this->ajaxReturn(['msg'=>$res,'status'=>'1']); //把手机号返
+    }
+
+    // 小程序解密
+    public function decryptData($encryptedData, $iv)
+    {
+        if (strlen($this->sessionKey) != 24) {
+            return self::$IllegalAesKey;
+        }
+        $aesKey=base64_decode($this->sessionKey);
         if (strlen($iv) != 24) {
-            return "error2";
+            return self::$IllegalIv;
         }
         $aesIV=base64_decode($iv);
         $aesCipher=base64_decode($encryptedData);
         $result=openssl_decrypt( $aesCipher, "AES-128-CBC", $aesKey, 1, $aesIV);
-        $dataObj=json_decode($result);
+        $dataObj=json_decode( $result );
         if( $dataObj  == NULL )
         {
-            return "aes 解密失败1";
+            return self::$IllegalBuffer;
         }
-        if( $dataObj->watermark->appid != $appid )
+        if( $dataObj->watermark->appid != self::$appid )
         {
-            return "aes 解密失败2";
+            return self::$IllegalBuffer;
         }
-        $data = $result;
-        return "OK";
+        return  $dataObj;
+        // return self::$OK;
     }
-//手机绑定传session_key
-    public function get_session_key(){
-        $encryptedData=I('post.encryptedData');
-        $iv=I('post.iv');
-        $openid=I('post.openid');
-        $appid='wxf348bbbcc28d7e10';
-        if($openid&&$appid&&$iv&&$encryptedData){
-            $sessionKey=urldecode('2501eb21dd9346f91e9b612b0097b50f');
-            $errCode = $this->decryptData($encryptedData, $iv, $data,$sessionKey,$appid);
-            if ($errCode == "OK") {
-                //获取用户绑定的手机号成功
-                print($data . "\n");
-            } else {
-                print($errCode . "\n");
-            }
-        }else{
-            $this->ajaxReturn(array('code' => 300, 'msg' => '参数缺失'));
-        }
+
+    function httpGet($url) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+        // 为保证第三方服务器与微信服务器之间数据传输的安全性，所有微信接口采用https方式调用，必须使用下面2行代码打开ssl安全校验。
+        // 如果在部署过程中代码在此处验证失败，请到 http://curl.haxx.se/ca/cacert.pem 下载新的证书判别文件。
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        $res = curl_exec($curl);
+        curl_close($curl);
+        return $res;
     }
+
 
 
 
