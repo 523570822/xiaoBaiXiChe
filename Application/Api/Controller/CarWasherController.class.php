d1bc3c54cbc5d267b2e6639b3d0be04d756be6f8
wangjiaming <459681469@qq.com>
2019-03-11 15:19:27 +0800

    去掉经纬度

--- Application/Api/Controller/CarWasherController.class.php
+++ Application/Api/Controller/CarWasherController.class.php
@@ -294,8 +294,8 @@ class CarWasherController extends BaseController
                         $offline = M('CarWasher')->where($off_where)->save($off_data);
                     }
                     foreach ($vv as $kk1=>$vv1){
-                        $car_data['lon'] = $vv1['queryitem']['location']['longitude'];
-                        $car_data['lat'] = $vv1['queryitem']['location']['latitude'];
+                        //$car_data['lon'] = $vv1['queryitem']['location']['longitude'];
+                        //$car_data['lat'] = $vv1['queryitem']['location']['latitude'];
                         $car_data['electricity'] = $vv1['queryitem']['device_energy'];
                         $car_data['water_volume'] = $vv1['queryitem']['clean_water_usage'];
                         $car_data['foam'] = $vv1['queryitem']['foam_usage'];
