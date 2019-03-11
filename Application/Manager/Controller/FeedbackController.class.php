4fd46999304bb2a4bec1dfd292f7888f8a67f670
谢吉发 <969318117@qq.com>
2019-03-11 11:28:42 +0800

    支付接口优化

--- Application/Manager/Controller/FeedbackController.class.php
+++ Application/Manager/Controller/FeedbackController.class.php
@@ -11,8 +11,6 @@ namespace Manager\Controller;
 
 class FeedbackController extends BaseController
 {
-
-
     /**
      * 意见反馈
      * User: admin
@@ -20,12 +18,43 @@ class FeedbackController extends BaseController
      */
     public function index()
     {
-        $where['status'] = array('lt',9);
-        $param['order'] = 'id  desc';
-        $param['page_size'] =15;
-        $data = D('Feedback')->queryList($where, '*',$param);
-        $this->assign($data);
-        $this->display();
+        $where = array ();
+        //按用户账号查找
+        if(!empty($_REQUEST['nickname'])){
+            $nickname_where['nickname'] = array('LIKE',"%".I('request.nickname')."%");
+            $data = D('Member')->where ($nickname_where)->getField("id", true);
+            $where["m_id"] = ["in", implode ($data, ',')];
+            if (empty($data))
+            {
+                $this->display();
+            }
+        }
+        //运行状态查找
+        if ( !empty($_REQUEST['status']) ) {
+            if ( $_REQUEST['status'] == 1 ) {
+                $where['status'] = 0;
+            } elseif ( $_REQUEST['status'] == 2 ) {
+                $where['status'] = 1;
+            }
+        }
+        if ( !$_REQUEST['status'] ) {
+            $where['status'] = array ('lt' , 9);
+        }
+        $param['page_size'] = 15;
+        $data = D ('Feedback')->queryList ($where , '*' , $param);
+        foreach ($data['list'] as $k=>$v){
+            $data['list'][$k]['contents']=$v['content'];
+            $data['list'][$k]['m_id']=$v['m_id'];
+            $data['list'][$k]['pro_id']=$v['pro_id'];
+            $dates = D('Member')->where (array ('id'=>$data['list'][$k]['m_id']))->field ('nickname')->find();
+            $date = D('Problem')->where (array ('id'=>$data['list'][$k]['pro_id']))->field ('content')->find();
+            $data['list'][$k]['nickname']=$dates['nickname'];
+            $data['list'][$k]['content']=$date['content'];
+        }
+        $this->assign ($data);
+        //页数跳转
+        $this->assign ('url' , $this->curPageURL ());
+        $this->display ();
     }
 
     public function saveFeedback()
@@ -33,7 +62,7 @@ class FeedbackController extends BaseController
         $id = $this->checkParam(array('id', 'int'));
         $status = D('Feedback')->where(array('id'=>$id))->getField('status');
         $data = $status == 1 ? array('status'=>0) : array('status'=>1);
-        $Res = D('Feedback')->where(array('id'=>$id))->save($data);
+        D('Feedback')->where(array('id'=>$id))->save($data);
         $this->apiResponse(1, $status ==1 ? '已处理' : "" ) ;
     }
 }
