2ea15fc6358258d8e294adb914778326b325675e
谢吉发 <969318117@qq.com>
2019-03-11 17:34:38 +0800

    “小程序图片

--- Application/Api/Controller/FaultController.class.php
+++ Application/Api/Controller/FaultController.class.php
@@ -51,19 +51,7 @@ class FaultController extends BaseController {
             $this->apiResponse ('0' , '找不到该机器');
         }
         if ( $request['type'] ) {
-            $upload = new \Think\Upload();// 实例化上传类
-            $upload->maxSize  =   3145728 ;// 设置附件上传大小
-            $upload->exts   =   array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-            $upload->rootPath =   './Uploads/'; // 设置附件上传根目录
-            $upload->savePath =   ''; // 设置附件上传（子）目录
-            // 上传文件
-            $info  =  $upload->upload();
-            if(!$info) {// 上传错误提示错误信息
-                $this->error($upload->getError());
-            }else{// 上传成功 获取上传文件信息
-                //插入到数据库中
-                $this->apiResponse ($info);
-            }
+            $this->apiResponse ($_FILES);
         } else {
             if ( empty($_FILES['pic_id']['name']) ) {
                 $this->apiResponse (0 , '请上传问题机器故障照片');
