fb008e51bedff2b0574b871ca056bf338b238885
wangjiaming <459681469@qq.com>
2019-04-28 16:07:33 +0800

    修改订单后台

--- Application/Manager/Controller/OrderController.class.php
+++ Application/Manager/Controller/OrderController.class.php
@@ -23,9 +23,14 @@ class OrderController extends BaseController
         if(!empty($_REQUEST['orderid'])){
             $where['orderid'] = array('LIKE',"%".I('request.orderid')."%");
         }
-        //按流水号查找
-        if(!empty($_REQUEST['trade_no'])){
-            $where['trade_no'] = array('LIKE',"%".I('request.trade_no')."%");
+        //按洗车机编号查找
+        if(!empty($_REQUEST['mc_code'])){
+            $account_where['mc_code'] = array('LIKE',I('request.mc_code')."%");
+            $data['id'] = D('CarWasher')->where ($account_where)->getField("id", true);
+            $where["c_id"] = ["in", implode ($data['id'], ',')];
+            if (empty($data)) {
+                $this->display();
+            }
         }
         //按用户账号查找
         if(!empty($_REQUEST['account'])){
@@ -52,13 +57,22 @@ class OrderController extends BaseController
         if (!$_REQUEST['status']){
             $where['status'] = array('lt',9);
         }
+        //注册时间查找
+        if(!empty($_REQUEST['start_time']) && !empty($_REQUEST['end_time'])){
+            $where['create_time'] =array('between',array(strtotime($_REQUEST['start_time']),strtotime($_REQUEST['end_time'])+86400));
+        }elseif(!empty($_REQUEST['start_time'])){
+            $where['create_time'] = array('egt',strtotime($_REQUEST['start_time']));
+        }elseif(!empty($_REQUEST['end_time'])){
+            $where['create_time'] = array('elt',strtotime($_REQUEST['end_time'])+86399);
+        }
         $param['page_size'] = 15;
         $data = D('Order')->queryList($where, '*',$param);
         foreach ($data['list'] as $k=>$v){
             $data['list'][$k]['m_id']=$v['m_id'];
             $date = D('Member')->where (array ('id'=>$data['list'][$k]['m_id']))->field ('account')->find();
             $data['list'][$k]['account']=$date['account'];
-            $data['list'][$k]['account']=$date['account'];
+            $mc_code = M('CarWasher')->where(array('id'=>$data['list'][$k]['c_id']))->field('mc_code')->find();
+            $data['list'][$k]['mc_code']=$mc_code['mc_code'];
         }
         $this->assign($data);
         //页数跳转
