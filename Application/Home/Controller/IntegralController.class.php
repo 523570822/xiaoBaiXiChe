ae0b2e998651dfa62b27efc180397d988c70b202
wangjiaming <459681469@qq.com>
2019-07-01 17:52:59 +0800

    修改风相连接5

--- Application/Home/Controller/IntegralController.class.php
+++ Application/Home/Controller/IntegralController.class.php
@@ -9,10 +9,7 @@ class IntegralController extends BaseController {
     //https://www.xiaojingxiche.com/Integral/integral?m_id=1 分享链接
     //https://www.xiaojingxiche.com/Integral/download 下载链接
     function integral(){
-        dump($_REQUEST);exit;
-
         $m_id=$_REQUEST['m_id'];
-
         $param['field'] = 'id as m_id,account,nickname,head_pic,degree,invite_code';
         if(!empty($m_id)){
             $member_info = M('Member')->where(array('id'=>$m_id))->field($param['field'])->find();
