b13c73552e0d7879a2987f944d93810aa0df818c
wangjiaming <459681469@qq.com>
2019-07-01 17:39:54 +0800

    修改分享

--- Application/Home/Controller/IntegralController.class.php
+++ Application/Home/Controller/IntegralController.class.php
@@ -10,8 +10,14 @@ class IntegralController extends BaseController {
     //https://www.xiaojingxiche.com/Integral/download 下载链接
     function integral(){
         $m_id=$_REQUEST['m_id'];
+
         $param['field'] = 'id as m_id,account,nickname,head_pic,degree,invite_code';
-        $member_info = M('Member')->where(array('id'=>$m_id))->field($param['field'])->find();
+        if(!empty($m_id)){
+            $member_info = M('Member')->where(array('id'=>$m_id))->field($param['field'])->find();
+        }else{
+            $token=$_REQUEST['token'];
+            $member_info = M('Member')->where(array('token'=>$token))->field($param['field'])->find();
+        }
         if(!$member_info) {
             $member_info['invite_code']="0";
         }
