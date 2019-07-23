928c927eadea21c699018e471e25b9613634521e
wangjiaming <459681469@qq.com>
2019-07-23 16:39:29 +0800

    修改接口12

--- Application/Api/Controller/MemberController.class.php
+++ Application/Api/Controller/MemberController.class.php
@@ -240,7 +240,11 @@ class MemberController extends BaseController
 //        $request['bind_id'] = 606;
 //        $request['account'] = 18635356092;
 //        $request['verify'] = 785846;
-
+        //检查短信验证码
+        $res = D('Sms')->checkVerify($request['account'], $request['verify'], 're_bind');
+        if ($res['error']) {
+            $this->apiResponse('0', $res['error']);
+        }
         $phone = M('Member')->where(array('account'=>$request['account'],'status'=>array('neq',9)))->find();
 
         unset($param);
@@ -253,11 +257,7 @@ class MemberController extends BaseController
         if (!$bind_info) {
             $this->apiResponse('0', '绑定手机号失败');
         }
-        //检查短信验证码
-        $res = D('Sms')->checkVerify(18635356092, 980694, 're_bind');
-        if ($res['error']) {
-            $this->apiResponse('0', $res['error']);
-        }
+
         unset($param);
         $param['where']['account'] = $request['account'];
         $param['where']['status'] = array('neq', 9);
